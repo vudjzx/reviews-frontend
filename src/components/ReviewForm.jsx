f53ba237837d08798c6d2256ed50ba6c192c7d64
Victor Duran <victor.duran@liberet.com>
2022-07-18 03:33:48 -0500

    added login form validation

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -7,6 +7,7 @@ import { GET_REVIEWS } from "../pages/Reviews";
 import { AuthContext } from "../context/AuthProvider";
 import { GET_MEDIA_REVIEWS } from "../pages/MediaReview";
 import { GET_PUBLIC_REVIEWS } from "../pages/Profile";
+import { Link } from "react-router-dom";
 
 const CREATE_REVIEW = gql`
   mutation CreateReview($reviewInput: ReviewInput!) {
@@ -101,43 +102,75 @@ export default function ReviewForm({
         <LoadingComponent />
       </div>
     );
+
   if (error) return `Submission error! ${error.message}`;
+
   return (
     <div className="pt-6 pb-32">
       <h2 className="py-4">Write a review</h2>
       {alert.message !== "" && (
         <CustomAlert message={alert.message} error={alert.error} />
       )}
-      {pristineForm && (
-        <form className="pb-8 mb-4">
-          <div className="mb-4">
-            <label className="block text-lg font-bold mb-2" htmlFor="review">
-              {`Your review *`}
-            </label>
-            <textarea
-              className="shadow appearance-none border rounded-lg w-full py-2 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline h-32 resize-none"
-              id="review"
-              type="text"
-              placeholder="This was a great movie..."
-              value={formData.content}
-              onChange={(e) =>
-                setFormData({ ...formData, content: e.target.value })
-              }
-            />
-          </div>
-          <p className="block text-lg font-bold mb-2">{`Score *`}</p>
-          <CustomListBox score={formData.score} setScore={setScore} />
-          <div className="w-full py-4">
-            <button
-              className="bg-indigo-600 hover:bg-indigo-800 text-white font-bold py-2 px-4 rounded-lg transition-colors w-full"
-              type="submit"
-              onClick={handleSubmit}
-            >
-              Submit review
-            </button>
-          </div>
-        </form>
-      )}
+      <FormViewSelector
+        formData={formData}
+        setFormData={setFormData}
+        pristineForm={pristineForm}
+        user={user}
+        setScore={setScore}
+        handleSubmit={handleSubmit}
+      />
     </div>
+  );
+}
+
+function FormViewSelector({
+  formData,
+  setFormData,
+  pristineForm,
+  user,
+  setScore,
+  handleSubmit,
+}) {
+  if (!user) {
+    return (
+      <div className="text-center py-16">
+        <Link to="/login" className="text-2xl">
+          <span className="text-indigo-500">Login</span> to write a review
+        </Link>
+      </div>
+    );
+  }
+
+  if (!pristineForm) return null;
+
+  return (
+    <form className="pb-8 mb-4">
+      <div className="mb-4">
+        <label className="block text-lg font-bold mb-2" htmlFor="review">
+          {`Your review *`}
+        </label>
+        <textarea
+          className="shadow appearance-none border rounded-lg w-full py-2 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline h-32 resize-none"
+          id="review"
+          type="text"
+          placeholder="This was a great movie..."
+          value={formData.content}
+          onChange={(e) =>
+            setFormData({ ...formData, content: e.target.value })
+          }
+        />
+      </div>
+      <p className="block text-lg font-bold mb-2">{`Score *`}</p>
+      <CustomListBox score={formData.score} setScore={setScore} />
+      <div className="w-full py-4">
+        <button
+          className="bg-indigo-600 hover:bg-indigo-800 text-white font-bold py-2 px-4 rounded-lg transition-colors w-full"
+          type="submit"
+          onClick={handleSubmit}
+        >
+          Submit review
+        </button>
+      </div>
+    </form>
   );
 }
