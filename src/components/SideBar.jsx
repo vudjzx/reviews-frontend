fccb0e1cacd10dcd2b2e0893305986dac6207536
Victor Duran <victor.duran@liberet.com>
2022-07-18 13:18:00 -0500

    fixed dark styles

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -21,7 +21,7 @@ const SideBar = () => {
   return (
     <div
       className="fixed top-0 left-0 h-screen w-16 flex flex-col
-                  bg-white dark:bg-gray-900 shadow-lg pt-24 z-50"
+                   bg-gray-900 shadow-lg pt-24 z-50"
     >
       <SideBarIcon
         route="/"
